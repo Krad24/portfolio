06aa38561601980c57c0241d218ad84c4be87cf4
Krad24 <marhipov240692@gmail.com>
2023-07-05 18:06:28 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,107 @@
+"use strict"
+
+// sliders
+
+const swiper2 = new Swiper('.portfolio__slider', {
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+  },
+  watchOverflow: true,
+  
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+});
+
+// sticky menu
+
+const fixedMenu = document.getElementById('fixedMenu');
+const nav = document.getElementById('fixedHeader');
+const classMenu = fixedMenu.classList.value;
+
+window.addEventListener('scroll', () => {
+  setTimeout(fun, 200);
+})
+
+function fun () {
+  if (window.pageYOffset > 10){
+    fixedMenu.classList.add(`${classMenu}--fixed`);
+    nav.classList.add('header--fixed')
+
+  } else {
+    fixedMenu.classList.remove(`${classMenu}--fixed`)
+    nav.classList.remove('header--fixed')
+  }
+}
+
+//end sticky menu
+
+//burger menu
+
+const burgerBtn = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.menu');
+const body = document.querySelector('#body');
+
+
+burgerBtn.addEventListener('click', (e)=> {
+  burgerBtn.classList.toggle(`${burgerBtn.classList[0]}--close`);
+  burgerMenu.classList.toggle(`${burgerMenu.classList[0]}--active`);
+  body.classList.toggle('lock');
+  
+  if (e.target.classList.value == 'lock') {
+    body.addEventListener('click', ()=> {
+      body.classList.remove('lock');
+      burgerBtn.classList.remove(`${burgerBtn.classList[0]}--close`);
+      burgerMenu.classList.remove(`${burgerMenu.classList[0]}--active`);
+    })
+  }
+})
+
+body.addEventListener('click', (e)=> {
+  if (e.target.classList.value == 'lock') {
+    body.classList.remove('lock');
+    burgerBtn.classList.remove(`${burgerBtn.classList[0]}--close`);
+    burgerMenu.classList.remove(`${burgerMenu.classList[0]}--active`);
+  }
+})
+
+
+//end burger menu
+
+
+// плавное перемещение к якорю header-menu
+
+const asideLinks = document.querySelectorAll('.menu__link');
+
+if (asideLinks) {
+  asideLinks.forEach(elem => scrollSmooth(elem))
+}
+
+// end плавное перемещение к якорю aside-menu
+
+// плавное перемещение к якорю 
+
+function scrollSmooth (elem) {
+  if(elem) {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      const blockID = elem.getAttribute('href');
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+      console.log('done')
+    })
+  } else {
+    console.log("Елемент не передан");
+  }
+} 
+
